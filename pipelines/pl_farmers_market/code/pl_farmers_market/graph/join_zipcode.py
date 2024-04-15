0c0c269bfca158748919b8fb139d6e0dc1122a2c
sharad.ghallal@cloudaeon.net <sharad.ghallal@cloudaeon.net>
2024-04-15 18:37:17 +0000

    ❇️ New     - Dataset        - ds_farmers_markets_analysis
    🔁 Modified
        - Pipeline
           - pl_farmers_market

--- pipelines/pl_farmers_market/code/pl_farmers_market/graph/join_zipcode.py
+++ pipelines/pl_farmers_market/code/pl_farmers_market/graph/join_zipcode.py
@@ -6,7 +6,7 @@ from prophecy.libs import typed_lit
 from pl_farmers_market.config.ConfigStore import *
 from pl_farmers_market.udfs.UDFs import *
 
-def by_zipcode(spark: SparkSession, in0: DataFrame, in1: DataFrame, ) -> DataFrame:
+def join_zipcode(spark: SparkSession, in0: DataFrame, in1: DataFrame, ) -> DataFrame:
     return in0\
         .alias("in0")\
         .join(in1.alias("in1"), (col("in0.zip") == col("in1.zipcode")), "inner")\
